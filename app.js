12baa3e062cf1867c4c4e56dcbc7a2a72082a356
denailry <daniel93ryan@gmail.com>
2018-01-23 08:18:32 +0700

    Fix: mysql connect problem

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 var express = require("express");
-var mysql = require("mysql");
 var fs = require("fs");
 var app = express();
 
-var host = fs.readFileSync('host.dat', 'utf8').split(",");
-if(host[2] == 0) {
-	host[2] = "";
-}
+const MYSQL_HOST = 0;
+const MYSQL_USER = 1;
+const MYSQL_PASS = 2;
+const MYSQL_PORT = 3;
 
+var host = fs.readFileSync('host.dat', 'utf8').split(",");
+if(host[MYSQL_PASS] == 0) {
+	host[MYSQL_PASS] = "";
+}
+if(host[MYSQL_PORT] == 0) {
+	host[MYSQL_PORT] = null;
+}
+var mysql = require("mysql");
 var con = mysql.createConnection({
-	host: host[0],
-	user: host[1],
-	password: host[2]
+	host: host[MYSQL_HOST],
+	user: host[MYSQL_USER],
+	password: host[MYSQL_PASS],
+	port: host[MYSQL_PORT]
 });
 var connect_msg = "Error!"
 con.connect(function(err) {
-	if(err) throw err;
-	connect_msg = "Connected!"
+	if(!err) {
+		connect_msg = "Connected!";
+	}
 });
-
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-	//res.render("home.ejs");
 	res.send(connect_msg);
 });
+app.listen(3000, () => {
+	console.log("Server On!");
+});
+
+/*
+app.use(express.static("public"));
 
 app.get("/fallinlovewith/:thing", (req, res) => {
 	var thing = req.params.thing;
@@ -78,7 +89,4 @@ app.get("/repeat/:words/:n", function(req, res) {
 app.get("*", function(req, res) {
 	res.send("ERROR BRO!");
 })
-
-app.listen(3000, () => {
-	console.log("Server On!");
-});
+*/
